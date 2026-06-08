0437ec7742f79e80c6319c659d05e6b6b63603c2
kavya shree <akavyashree.3009@gmail.com>
2026-06-09 01:24:25 +0530

    module 1

---
+++ python/lesson 2/buffer.py
@@ -0,0 +1,17 @@
+# Take user inputs
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+
+# Print a formatted name tag
+print("\n--- NAME TAG ---")
+print("---------------")
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+
+# Loading dots using end=
+print("\nLoading", end="")
+print(".", end="")
+print(".", end="")
+print(".")
