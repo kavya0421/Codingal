5dac405800d1723ce8b6ac823d9e1df58183dcd2
kavya shree <akavyashree.3009@gmail.com>
2026-06-09 02:12:58 +0530

    module 2

---
+++ python/module 2/lesson 6/ACP.py
@@ -0,0 +1,23 @@
+import turtle
+
+turtle.Screen().bgcolor("Aqua")
+
+board = turtle.Turtle()
+
+# first side of square
+board.forward(100)
+board.left(90)
+
+# second side of square
+board.forward(100)
+board.left(90)
+
+# third side of square
+board.forward(100)
+board.left(90)
+
+# fourth side of square
+board.forward(100)
+board.left(90)
+
+turtle.done()
